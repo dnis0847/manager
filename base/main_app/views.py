92e19ece684cd84352a53e00b9a7b1dd27d4bc75
dnis0847 <78086750+dnis0847@users.noreply.github.com>
2025-01-22 22:52:17 +0700

    Создание страницы index.html + первая вьюха, создание базового шаблона.

--- base/main_app/views.py
+++ base/main_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from . models import Project, Task, Comment, UserProfile
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'main_app/index.html')
+    title = 'Projects Box | Главная'
+    projects = Project.objects.all()
+    return render(request, 'main_app/index.html', {'projects': projects, 'title': title})
